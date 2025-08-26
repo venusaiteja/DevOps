d5d94a2a978ff565c5c1dca1d16fe1607cce9c5d
R Sandeep <venusaiteja.11@gmail.com>
2025-08-26 10:30:53 +0530

    this is a python project

---
+++ PythonProject/main.py
@@ -0,0 +1,2 @@
+name = "Sandeep"
+print(name)
---
+++ PythonProject2/index.py
@@ -0,0 +1,15 @@
+name = "sandeep"
+print("Hello",name)
+print(type(name))
+
+
+age = 21
+age += 1
+print("your age is :"+str(age))
+
+
+height = 170.5
+height += 15
+print("your height is :" + str(height))
+
+
